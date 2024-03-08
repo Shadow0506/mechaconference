31d0d54dd0e92e35b3188bb3c0cf4aba2b5a47eb
Arnav Bansal <70932975+arnavbansal2764@users.noreply.github.com>
2024-03-08 15:02:29 +0530

    opt

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import {Suspense,lazy} from 'react'
-import { Header } from './Components/Header'
 import { Impdates } from './Components/Impdates'
 import { Home } from './Components/Home'
 import { Authors } from './Components/Authors'
@@ -14,17 +12,6 @@ import { Sponsorship } from './Components/Sponsorship'
 import { Venue } from './Components/Venue'
 import { Footer } from './Components/Footer'
 import {Headertailwind} from './Components/Headertailwind'
-const  impdates = lazy(()=>import('./Components/Impdates'))
-const  home = lazy(()=>import('./Components/Home'))
-const  authors = lazy(()=>import('./Components/Authors'))
-const  aboutus = lazy(()=>import('./Components/AboutUs'))
-const  callforpapers = lazy(()=>import('./Components/Callforpapers'))
-const  committees = lazy(()=>import('./Components/Committees'))
-const  contactus = lazy(()=>import('./Components/ContactUs'))
-const  registration = lazy(()=>import('./Components/Registration'))
-const  sponsorship = lazy(()=>import('./Components/Sponsorship'))
-const  venue = lazy(()=>import('./Components/Venue'))
-const  footer = lazy(()=>import('./Components/Footer'))
 function App() {
   
   return (
@@ -34,16 +21,16 @@ function App() {
     
     <Headertailwind />
     <Routes>
-        <Route path='/Impdates' element={<Suspense fallback={"loading.."}><Impdates /></Suspense>}></Route>
-        <Route path='/' element = {<Suspense fallback={"loading.."}><Home/></Suspense>} />
-        <Route path='/speakers' element = {<Suspense fallback={"loading.."}><Authors/></Suspense>} />
-        <Route path='/aboutus' element = {<Suspense fallback={"loading.."}><AboutUs/></Suspense>} />
-        <Route path='/callforpapers' element = {<Suspense fallback={"loading.."}><Callforpapers/></Suspense>} />
-        <Route path='/committees' element = {<Suspense fallback={"loading.."}><Committees/></Suspense>} />
-        <Route path='/contactus' element = {<Suspense fallback={"loading.."}><ContactUs/></Suspense>}/>
-        <Route path='/registration' element = {<Suspense fallback={"loading.."}><Registration/></Suspense>} />
-        <Route path='/sponsorship' element = {<Suspense fallback={"loading.."}><Sponsorship/></Suspense>} />
-        <Route path='/venue' element = {<Suspense fallback={"loading.."}><Venue/></Suspense>} />
+        <Route path='/Impdates' element={<Impdates />}></Route>
+        <Route path='/' element = {<Home/>} />
+        <Route path='/speakers' element = {<Authors/>} />
+        <Route path='/aboutus' element = {<AboutUs/>} />
+        <Route path='/callforpapers' element = {<Callforpapers/>} />
+        <Route path='/committees' element = {<Committees/>} />
+        <Route path='/contactus' element = {<ContactUs/>}/>
+        <Route path='/registration' element = {<Registration/>} />
+        <Route path='/sponsorship' element = {<Sponsorship/>} />
+        <Route path='/venue' element = {<Venue/>} />
       </Routes>
     <Footer />
     </BrowserRouter>
