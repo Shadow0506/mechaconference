b6b21bd4c6cd417c495197c8855f7cfee07cd393
Arnav Bansal <70932975+arnavbansal2764@users.noreply.github.com>
2024-03-08 12:35:07 +0530

    update

--- mechaconference/src/Components/Footer.jsx
+++ mechaconference/src/Components/Footer.jsx
@@ -1,15 +1,15 @@
 export function Footer() {
     return (
         <div>
-            
 
-<footer class="bg-white shadow dark:bg-gray-900 ">
-    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" class="hover:underline">ICDMT 2024</a>. All Rights Reserved.Designed by Punjab Engineering College (Deemed to be University), Chandigarh.</span>
-    </div>
-</footer>
+
+            <footer class="bg-white shadow dark:bg-gray-900 ">
+                <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+
+                    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+                    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" class="hover:underline">ICDMT 2024</a>. All Rights Reserved.Designed by Punjab Engineering College (Deemed to be University), Chandigarh.</span>
+                </div>
+            </footer>
 
 
         </div>
