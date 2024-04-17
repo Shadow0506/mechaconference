05d7a94e6f80a52c9f003e5123a0121770c93ef5
Bhavya Pratap Singh <145379279+Shadow0506@users.noreply.github.com>
2024-04-18 01:32:01 +0530

    Update Callforpapers.jsx

--- src/Components/Callforpapers.jsx
+++ src/Components/Callforpapers.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 export function Callforpapers() {
     const navigate = useNavigate();
     return (
-        <div>
+        <div style={{backgroundColor: '#E9FFFF'}}>
             <section className="text-gray-600 body-font">
                 <div className="container px-5 py-24 mx-auto">
                     <div className="text-center mb-20">
