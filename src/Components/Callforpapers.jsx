6098ef829547819ba017b79262ca3702f23bf068
Arnav Bansal <70932975+arnavbansal2764@users.noreply.github.com>
2024-05-26 22:59:39 +0530

    Updated Buttons

--- src/Components/Callforpapers.jsx
+++ src/Components/Callforpapers.jsx
@@ -353,13 +353,13 @@ export function Callforpapers() {
                             <span className="texto">Download Template</span>
                         </button>
                         <a
-                            href="https://cmt3.research.microsoft.com/ICDMT2024/Submission/Index" target="_blank"
+                           href="https://cmt3.research.microsoft.com/docs/help/pdfs/small_conference_guide.pdf" target="_blank"
                             className="inline-block px-3 py-2 mb-2 text-white bg-green-500 border border-transparent rounded-md shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-white" style={{ backgroundColor: '#818589' }}
                         >
                             Guide for Authors
                         </a>
                         <a
-                            href="https://cmt3.research.microsoft.com/docs/help/pdfs/small_conference_guide.pdf" target="_blank"
+                           href="https://cmt3.research.microsoft.com/ICDMT2024/Submission/Index"  target="_blank"
                             className="inline-block px-3 py-2 mb-2 text-white bg-green-500 border border-transparent rounded-md shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-white"
                         >
                             Submit Your Paper
